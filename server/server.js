a24e6377d08cca11043d9d47fde8e01c798d16ae
Chris Were <chris@mozzler.com.au>
2020-03-11 09:43:50 +1030

    Run a local server that supports CORS

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+// Set up the express app
+const app = express();
+
+let corsConfig = {
+  //origin: process.env.CORS_HOST
+};
+
+// Parse incoming requests data
+app.use(cors(corsConfig));
+app.use(express.static("schemas/"));
+
+const PORT = 5010;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
