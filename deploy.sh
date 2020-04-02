2ce26d7f0a8d8a28423d108d4fa54765aed34e5e
Chris Were <chris@mozzler.com.au>
2020-04-02 20:16:01 +1030

    Add invalidation

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 aws s3 sync schemas s3://schemas.testnet.verida.io --profile verida-testnet --delete
-#aws cloudfront create-invalidation --distribution-id E2V62XFHX8HKC5 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E2DU6UUUVFKZWK --paths "/*"
