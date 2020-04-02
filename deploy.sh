61385504804a454a667f4018f6d06f4bde0df76e
Chris Were <chris@mozzler.com.au>
2020-04-02 19:39:21 +1030

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-aws s3 sync schemas s3://schemas.alpha.verida.io --delete
+aws s3 sync schemas s3://schemas.testnet.verida.io --profile verida-testnet --delete
+#aws cloudfront create-invalidation --distribution-id E2V62XFHX8HKC5 --paths "/*"
