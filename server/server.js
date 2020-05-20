e9442541e68f8036b7a2dc9cb702b51efeb8fceb
Chris Were <chris@mozzler.com.au>
2020-05-20 12:09:59 +0930

    Force schema+json

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ let corsConfig = {
 
 // Parse incoming requests data
 app.use(cors(corsConfig));
-app.use(express.static("schemas/"));
+app.use(express.static("schemas/", {
+  setHeaders: function(res, path, stat) {
+    if (path.substring(path.length-4) == 'json') {
+      res.type('application/schema+json')
+    }
+  }
+}));
 
 const PORT = 5010;
 app.listen(PORT, () => {
