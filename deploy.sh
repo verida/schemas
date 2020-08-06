30f162db3d36a1e4cfde68425539af774fbd4a51
Chris Were <chris@mozzler.com.au>
2020-08-06 14:39:18 +0930

    Fix deployment to force correct content-type of schema

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-aws s3 sync schemas s3://schemas.testnet.verida.io --profile verida-testnet --delete
+aws s3 sync schemas s3://schemas.testnet.verida.io --content-type application/schema+json --profile verida-testnet --delete
 aws cloudfront create-invalidation --profile verida-testnet --distribution-id E2DU6UUUVFKZWK --paths "/*"
